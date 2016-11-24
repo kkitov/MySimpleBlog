485a2da19b487064cc1d9bcb30656746583ad396
Kristiyan <kriskoyyy@abv.bg>
2016-11-24 10:52:02 +0200

    First commit

---
+++ config/routes.js
@@ -0,0 +1,51 @@
+const userController = require('./../controllers/user');
+const homeController = require('./../controllers/home');
+const articleController =require('./../controllers/article');
+const adminController = require('./../controllers/admin/admin');
+
+module.exports = (app) => {
+    app.get('/', homeController.index);
+
+    app.get('/user/register', userController.registerGet);
+    app.post('/user/register', userController.registerPost);
+
+    app.get('/user/login', userController.loginGet);
+    app.post('/user/login', userController.loginPost);
+
+    app.get('/user/logout', userController.logout);
+
+    app.get('/article/create',articleController.createGet);
+    app.post('/article/create',articleController.createPost);
+
+    app.get('/article/details/:id',articleController.details);
+
+    app.get('/article/edit/:id', articleController.editGet);
+    app.post('/article/edit/:id', articleController.editPost);
+
+    app.get('/article/delete/:id', articleController.deleteGet);
+    app.post('/article/delete/:id', articleController.deletePost);
+
+    app.use((req,res,next)=>{
+        if(req.isAuthenticated()){
+            req.user.isInRole('Admin').then(isAdmin=>{
+                if(isAdmin){
+                    next();
+                }else{
+                    res.redirect('/');
+                }
+            })
+
+        }else{
+            res.redirect('/user/login');
+        }
+    });
+    app.get('/admin/user/all', adminController.user.all);
+
+    app.get('/admin/user/edit/:id',adminController.user.editGet);
+    app.post('/admin/user/edit/:id',adminController.user.editPost);
+
+    app.get('/admin/user/delete/:id',adminController.user.deleteGet);
+    app.post('/admin/user/delete/:id',adminController.user.deletePost);
+
+};
+
---
+++ controllers/article.js
@@ -0,0 +1,115 @@
+const Article = require('mongoose').model('Article');
+
+module.exports = {
+    createGet: (req, res)=> {
+        res.render('article/create');
+    },
+    createPost: (req, res) => {
+        let articleArgs = req.body;
+
+
+        let errorMsg = '';
+        if (!req.isAuthenticated()) {
+            errorMsg = 'You should be logged in to make articles!'
+        } else if (!articleArgs.title) {
+            errorMsg = 'Invalid title!';
+        } else if (!articleArgs.content) {
+            errorMsg = 'Invalid content!';
+        }
+
+        if (errorMsg) {
+            res.render('article/create', {error: errorMsg});
+            return;
+        }
+        articleArgs.author = req.user.id;
+        Article.create(articleArgs).then(article=> {
+          article.prepareInsert();
+            res.redirect('/');
+        });
+    },
+    details: (req,res) => {
+        let id = req.params.id;
+
+        Article.findById(id).populate('author').then(article =>{
+            if (!req.user){
+                res.render('article/details', {article: article, isUserAuthorized: false});
+                return;
+            }
+            req.user.isInRole('Admin').then(isAdmin => {
+                let isUserAuthorized = isAdmin || req.user.isAuthor(article);
+                res.render('article/details', {article:article, isUserAuthorized: isUserAuthorized});
+            });
+        });
+    },
+    editGet: (req, res) => {
+        let id = req.params.id;
+
+        if(!req.isAuthenticated()){
+            let returnUrl = `/article/edit/${id}`;
+            req.session.returnUrl = returnUrl;
+
+            res.redirect('/user/login');
+            return;
+        }
+
+        Article.findById(id).then(article => {
+            req.user.isInRole('Admin').then(isAdmin => {
+                if (!isAdmin && !req.user.isAuthor(article)){
+                    res.redirect('/');
+                    return;
+                }
+                res.render('article/edit', article)
+            });
+        });
+    },
+    editPost: (req, res) => {
+        let id = req.params.id;
+
+        let articleArgs = req.body;
+
+        let errorMsg = '';
+        if (!articleArgs.title) {
+            errorMsg = 'Article title cannot be empty!';
+        }
+        if (errorMsg) {
+            res.render('article/edit', {error: errorMsg})
+        } else {
+            Article.update({_id: id}, {
+                $set: {
+                    title: articleArgs.title,
+                    content: articleArgs.content
+                }
+            }).then(updateStatus => {
+                res.redirect(`/article/details/${id}`);
+            })
+        }
+    },
+    deleteGet: (req, res) => {
+        let id = req.params.id;
+        if (!req.isAuthenticated()){
+            let returnUrl = `/article/delete/${id}`;
+            req.session.returnUrl = returnUrl;
+
+            res.redirect('/user/login');
+            return;
+        }
+
+        Article.findById(id).then(article => {
+            req.user.isInRole('Admin').then(isAdmin => {
+                if(!isAdmin && !req.user.isAuthor(article)){
+                    res.redirect('/');
+                    return;
+                }
+                res.render('article/delete',article)
+            });
+        });
+    },
+    deletePost:(req,res) => {
+        let id = req.params.id;
+        Article.findOneAndRemove({_id:id}).populate('author').then(article => {
+            article.prepareDelete();
+            res.redirect('/');
+        });
+
+    }
+};
